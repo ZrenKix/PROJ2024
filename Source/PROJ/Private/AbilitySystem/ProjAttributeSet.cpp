55344e24e8c566427328a2bf518ec325278669c3
shittyGameDev <129396037+shittyGameDev@users.noreply.github.com>
2024-12-04 11:40:07 +0100

    wip

--- Source/PROJ/Private/AbilitySystem/ProjAttributeSet.cpp
+++ Source/PROJ/Private/AbilitySystem/ProjAttributeSet.cpp
@@ -28,6 +28,9 @@ void UProjAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimePropert
 	// Secondary Attributes
 	DOREPLIFETIME_CONDITION_NOTIFY(UProjAttributeSet, Armor, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UProjAttributeSet, BlockChance, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UProjAttributeSet, CriticalHitChance, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UProjAttributeSet, CriticalHitDamage, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UProjAttributeSet, CriticalHitResistance, COND_None, REPNOTIFY_Always);
 
 	DOREPLIFETIME_CONDITION_NOTIFY(UProjAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UProjAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
@@ -192,6 +195,19 @@ void UProjAttributeSet::OnRep_Armor(const FGameplayAttributeData& OldArmor) cons
 void UProjAttributeSet::OnRep_BlockChance(const FGameplayAttributeData& OldBlockChance) const
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UProjAttributeSet, BlockChance, OldBlockChance);
+}
+
+void UProjAttributeSet::OnRep_CriticalHitChance(const FGameplayAttributeData& OldCriticalHitChance) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UProjAttributeSet, CriticalHitChance, OldCriticalHitChance);
+}
+void UProjAttributeSet::OnRep_CriticalHitDamage(const FGameplayAttributeData& OldCriticalHitDamage) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UProjAttributeSet, CriticalHitDamage, OldCriticalHitDamage);
+}
+void UProjAttributeSet::OnRep_CriticalHitResistance(const FGameplayAttributeData& OldCriticalHitResistance) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UProjAttributeSet, CriticalHitResistance, OldCriticalHitResistance);
 }
 
 
